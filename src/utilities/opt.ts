cdc7d3115dd1ce7c66c6d9b8a9b420e1475c67bb
Karim Shehadeh <kshehadeh@underarmour.com>
2024-01-22 04:48:57 -0500

    Run prettier on everything before commit

--- src/utilities/opt.ts
+++ src/utilities/opt.ts
@@ -3,7 +3,7 @@
  * @param o
  * @returns
  */
-export function opt(o?: Record<string, unknown>): Record<string, unknown> {    
+export function opt(o?: Record<string, unknown>): Record<string, unknown> {
     return Object.fromEntries(
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         Object.entries(o ?? {}).filter(([_, v]) => v !== undefined),
